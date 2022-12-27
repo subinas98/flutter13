45db89d2fe995a0a04ab2aa3ef466dcdfe44d808
subinas98@gmail.com <subinas98@gmail.com>
2022-12-27 11:05:40 +0530

    Files added commit

---
+++ todoapp/lib/MyNotes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyNotes extends StatefulWidget {
+  const MyNotes({super.key});
+
+  @override
+  State<MyNotes> createState() => _MyNotesState();
+}
+
+class _MyNotesState extends State<MyNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Todo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.yellow),
+      home: MyNotes(),
+    );
+  }
+}
